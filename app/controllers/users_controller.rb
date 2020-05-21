a2e939b69c8ade32586e8aa868cd7de0c8456a6d
Moin <kmoin786@gmail.com>
2020-05-22 03:58:41 +0530

    create edit functionality to re-former

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+
+    else
+      render :edit
+    end
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
